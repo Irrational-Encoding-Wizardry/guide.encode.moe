0dcda6d3b1049e47e68131377dd7125dae21e8ce
FichteFoll <fichtefoll2@googlemail.com>
2018-03-16 16:16:08 +0100

    Initalizie ssh-agent
    And minor adjustments.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,14 @@ if [ $TRAVIS_BRANCH = master ]; then
   # create commit of _book folder
   cp CNAME _build
   cd _book
+  rm .gitignore
   git init
   git add .
-  git -c user.name='Deployment Bot' -c user.email='deploy@travis-ci.org' commit -m "Auto-deploy from Travis CI"
+  git -c user.name='Deployment Bot' -c user.email='deploy@travis-ci.org' commit -m "Auto-deploy of $TRAVIS_COMMIT"
 
   # push to gh-pages branch
   openssl aes-256-cbc -K $encrypted_81bb60eb1b21_key -iv $encrypted_81bb60eb1b21_iv -in id_deploy.enc -out .travis/id_deploy -d
+  ssh-agent
   ssh-add .travis/id_deploy
   git push -f -q "ssh://git@github.com/$TRAVIS_REPO_SLUG.git" gh-pages
 fi
