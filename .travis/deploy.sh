a1f70018f8cfa9272b02ef495f7d4b6fcb411957
FichteFoll <fichtefoll2@googlemail.com>
2018-03-16 16:17:15 +0100

    Unwrap if so that `set -v` makes sense

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,18 +2,20 @@
 
 set -ev # show commands run and terminate on non-zero exit code
 
-if [ $TRAVIS_BRANCH = master ]; then
-  # create commit of _book folder
-  cp CNAME _build
-  cd _book
-  rm .gitignore
-  git init
-  git add .
-  git -c user.name='Deployment Bot' -c user.email='deploy@travis-ci.org' commit -m "Auto-deploy of $TRAVIS_COMMIT"
-
-  # push to gh-pages branch
-  openssl aes-256-cbc -K $encrypted_81bb60eb1b21_key -iv $encrypted_81bb60eb1b21_iv -in id_deploy.enc -out .travis/id_deploy -d
-  ssh-agent
-  ssh-add .travis/id_deploy
-  git push -f -q "ssh://git@github.com/$TRAVIS_REPO_SLUG.git" gh-pages
+if [ $TRAVIS_BRANCH != master ]; then
+  exit 0
 fi
+
+# create commit of _book folder
+cp CNAME _build
+cd _book
+rm .gitignore
+git init
+git add .
+git -c user.name='Deployment Bot' -c user.email='deploy@travis-ci.org' commit -m "Auto-deploy of $TRAVIS_COMMIT"
+
+# push to gh-pages branch
+openssl aes-256-cbc -K $encrypted_81bb60eb1b21_key -iv $encrypted_81bb60eb1b21_iv -in id_deploy.enc -out .travis/id_deploy -d
+ssh-agent
+ssh-add .travis/id_deploy
+git push -f -q "ssh://git@github.com/$TRAVIS_REPO_SLUG.git" gh-pages
