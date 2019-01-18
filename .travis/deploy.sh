a9d0e35750973395967728fc1137f14cf0bd9c13
FichteFoll <fichtefoll2@googlemail.com>
2019-01-18 02:49:06 +0100

    Fix auto-deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,16 +7,18 @@ if [[ $TRAVIS_BRANCH != master || $TRAVIS_PULL_REQUEST != false ]]; then
 fi
 
 # create commit of _book folder
-cp CNAME _build
-cd _book
-rm .gitignore
+pushd _book
 git init
 git add .
 git -c user.name='Deployment Bot' -c user.email='deploy@travis-ci.org' commit -m "Auto-deploy of $TRAVIS_COMMIT"
+popd
 
-# push to gh-pages branch
+# load ssh key
 openssl aes-256-cbc -K $encrypted_81bb60eb1b21_key -iv $encrypted_81bb60eb1b21_iv -in .travis/id_deploy.enc -out .travis/id_deploy -d
 chmod 600 .travis/id_deploy
 eval $(ssh-agent -s)
 ssh-add .travis/id_deploy
+
+# push to gh-pages branch
+cd _book
 git push -f -q "ssh://git@github.com/$TRAVIS_REPO_SLUG.git" HEAD:gh-pages
